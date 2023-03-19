8635b3b04e552632a9895f12ec6cfae6c04f61b8
Pedro Andrés urrutia Hurtarte <61337196+Urruthia@users.noreply.github.com>
2023-03-18 23:17:35 -0600

    Comit
    Mi comit

---
+++ SimulacionDeProcesos/src/main/java/classes/Proceso.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author palit
+ */
+public class Proceso {
+    private Integer tamanio;
+    private Integer tiempo;
+    private Integer proceso;
+
+ 
+    private String planificador;
+    private String base;
+    private String limite;
+    private String hexadecimal;
+    private int posicion;
+    private int sumaT;
+   
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+    
+    public Proceso(Integer min) {
+        this.tiempo = sumaT;
+        this.tamanio = tiempo*90; // El maximo de la memoria principal es de 30 segundos
+        this.hexadecimal= Integer.toHexString(this.tamanio);
+    }
+
+    public String getHexadecimal() {
+        return hexadecimal;
+    }
+
+    public Integer getTiempo() {
+        return tiempo;
+    }
+    
+    public void setTiempo(Integer tiempo) {
+        this.tiempo = tiempo;
+    }
+    
+    public Integer getProceso() {
+        return proceso;
+    }
+
+    public void setProceso(Integer proceso) {
+        this.proceso = proceso;
+    }
+    
+    public Integer getTamanio() {
+        return tamanio;
+    }
+ 
+    public void setTamanio(Integer tamanio){
+        this.tamanio = tamanio;
+    }
+    
+    public Integer getTamanioEnPix() {
+        return (tamanio*300/2000);
+    }
+    public void setBasehexa(Integer posicion)
+    {
+            this.base=Integer.toHexString(posicion);
+    }
+    public String getBasehexa()
+    {
+           return base;
+    }
+      public void setLimitehexa(Integer posicion,Integer tamanio)
+    {
+            int limit;
+            limit=posicion+tamanio;
+            this.limite=Integer.toHexString(limit);
+    }
+    public String getLimitehexa()
+    {
+           return limite;
+    }
+       public String getPlanificador() {
+        return planificador;
+    }
+
+    public void setPlanificador(String planificador) {
+        this.planificador = planificador;
+    }
+}
---
+++ SimulacionDeProcesos/src/main/java/classes/controlR.java
@@ -0,0 +1,42 @@
+package classes;
+
+/**
+ *
+ * @author Fernanda
+ */
+public class controlR {
+    private String P;
+    private String TC;
+    private String TL;
+
+    public controlR() {
+        this.P = P;
+        this.TC = TC;
+        this.TL = TL;
+    }
+
+    public String getP() {
+        return P;
+    }
+
+    public void setP(String P) {
+        this.P = P;
+    }
+
+    public String getTC() {
+        return TC;
+    }
+
+    public void setTC(String TC) {
+        this.TC = TC;
+    }
+
+    public String getTL() {
+        return TL;
+    }
+
+    public void setTL(String TL) {
+        this.TL = TL;
+    }
+    
+}
